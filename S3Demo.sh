d35e8bec77a972993d0857cb4377a50b898819cb
Aartee Kasliwal <aarteekasliwal@gmail.com>
2017-10-11 14:54:07 -0700

    Update S3Demo.sh

--- S3Demo.sh
+++ S3Demo.sh
@@ -7,6 +7,6 @@ aws s3 ls $S3Bucket
 #copy local file to the S3 bucket
 aws s3 cp ./s3.text s3://svcc-2017-demo-aartee1
 
-#sync the content of
+#sync the local directories on the S3 Bucket
 aws s3 sync ./files s3://svcc-2017-demo-aartee1/files
 
